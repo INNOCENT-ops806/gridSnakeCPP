8d7d59fd018691780c5c08300ae0214912dfc592
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-05-21 16:42:29 +0200

    add the body of the snake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 using namespace std;
 
 Color green = {173, 204, 96, 255};
-Color darkGreen = {123, 50, 250, 235};
+Color purple = {123, 50, 250, 235};
 
 int cellSize = 30;
 int cellCount = 25;
+
+class Snake{
+
+public:
+  deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+  void Draw(){
+    for(unsigned int i = 0; i < body.size(); i++){
+      float x = body[i].x;
+      float y = body[i].y;
+      Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, float(cellSize)};
+      DrawRectangleRounded(segment, 0.5, 6, purple);
+
+    }
+  }
+};
+
 class Food{
+
 public:
   Vector2 position;
 
@@ -16,8 +34,8 @@ public:
 
   };
 
-  void draw(){
-    DrawRectangle(position.x * cellSize,position.y * cellSize,cellSize,cellSize,darkGreen);
+  void Draw(){
+    DrawRectangle(position.x * cellSize,position.y * cellSize,cellSize,cellSize,purple);
   }
 
   Vector2 GenerateRandomPos(){
@@ -30,16 +48,19 @@ public:
 };
 
 int main() {
+
   cout << "Starting the game" << endl;
 
   InitWindow(cellSize * cellCount,cellSize * cellCount, "Retro Snake");
   SetTargetFPS(60);
   Food food = Food();
+  Snake snake = Snake();
 
   while (WindowShouldClose() == false) {
     BeginDrawing();
     ClearBackground(green);
-    food.draw();
+    food.Draw();
+    snake.Draw();
     EndDrawing();
   } 
 
