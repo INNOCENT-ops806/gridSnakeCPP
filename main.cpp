c13a260d5d5be91d976ef06272df1b4150743362
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-05-22 00:20:21 +0200

    added snake movement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <raylib.h>
 #include <deque>
+#include <raymath.h>
+
 using namespace std;
 
 Color green = {173, 204, 96, 255};
@@ -8,11 +10,24 @@ Color purple = {123, 50, 250, 235};
 
 int cellSize = 30;
 int cellCount = 25;
+double lastUpdateTime  = 0.0;
+
+bool eventTriggered(double interval){
+
+  double currentTime  = GetTime();
+  if ( currentTime  - lastUpdateTime >= interval) {
+    lastUpdateTime = currentTime;
+    return true;
+  }
+  return false;
+}
 
 class Snake{
 
 public:
   deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+  Vector2 direction = {1,0};
+
   void Draw(){
     for(unsigned int i = 0; i < body.size(); i++){
       float x = body[i].x;
@@ -22,12 +37,20 @@ public:
 
     }
   }
+
+  void update(){
+    body.pop_back();
+    body.push_front(Vector2Add(body[0], direction));
+  }
+
 };
+
 
 class Food{
 
 public:
   Vector2 position;
+
 
   Food(){
     position = GenerateRandomPos();
@@ -55,9 +78,32 @@ int main() {
   SetTargetFPS(60);
   Food food = Food();
   Snake snake = Snake();
+  snake.update();
 
   while (WindowShouldClose() == false) {
+
     BeginDrawing();
+
+    if (eventTriggered(0.2)) {
+      snake.update(); 
+    }
+
+    if (IsKeyPressed(KEY_UP)) {
+      snake.direction = {0,-1};
+    }
+
+    if (IsKeyPressed(KEY_DOWN)) {
+      snake.direction = {0,1};
+    }
+
+    if (IsKeyPressed(KEY_LEFT)) {
+      snake.direction = {-1,0}; 
+    }
+
+    if (IsKeyPressed(KEY_RIGHT)) {
+      snake.direction = {1,0}; 
+    }
+
     ClearBackground(green);
     food.Draw();
     snake.Draw();
@@ -67,4 +113,3 @@ int main() {
   CloseWindow(); 
   return 0;
 }
-
