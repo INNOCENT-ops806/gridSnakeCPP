348aa70756f1e61270a3b547739ff6fad040314a
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-11-19 13:51:44 +0200

    refactor: changed Snake.cpp and main.cpp to use the new static file's header file

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include "../include/Config.h"
+#include "../include/Game.h"
+#include "../include/Splash.h"
+
+int main() {
+  int const screenwidth = 1366;
+  int const screenheight = 760;
+
+  InitWindow(screenwidth, screenheight, "Retro Snake Game");
+  SetTargetFPS(60);
+
+  if (!IsWindowFullscreen()) {
+    ToggleFullscreen();
+  }
+
+  bool splashComplete = false;
+  Splash *splash = new Splash();
+  Game *game = nullptr;
+
+  while (!WindowShouldClose() && !IsKeyPressed(KEY_Q)) {
+    if (!splashComplete) {
+      if (splash->isComplete()) {
+        splashComplete = true;
+        delete splash;
+        splash = nullptr;
+        game = new Game();
+      }
+    } else {
+      if (game->menu.IsComplete() == false) {
+        game->menu.Update();
+      } else {
+        game->Update();
+      }
+    }
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    if (!splashComplete) {
+      splash->draw();
+    } else if (game->menu.IsComplete() == false) {
+      game->menu.Draw();
+    } else {
+      game->Draw();
+    }
+
+    EndDrawing();
+  }
+  if (game != nullptr) {
+    delete game;
+  }
+  if (splash != nullptr) {
+    delete splash;
+  }
+  CloseWindow();
+  return 0;
+}
